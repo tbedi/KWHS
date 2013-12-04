a3a8bfd38e54fdecbd0cb15fe32e18cc8c4448ea
Shriram <Shriram@kraususa.com>
2013-12-04 20:27:25 +0530

    add AuditDTO in Krause Services

---
+++ KrausWarehouseServices/KrausWarehouseServices/DTO/Shipping/AutditDTO.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KrausWarehouseServices.Connections.Shipping;
+using System.Runtime.Serialization;
+
+namespace KrausWarehouseServices.DTO.Shipping
+{
+    [DataContract]
+    class AutditDTO
+    {
+        /// <summary>
+        /// Blank Cunstructor.
+        /// </summary>
+        public AutditDTO()
+        { 
+        
+        }
+
+        /// <summary>
+        /// Paramiterised Constructor
+        /// </summary>
+        /// <param name="_audit">
+        /// pass The Audit table object
+        /// </param>
+        public AutditDTO(Audit _audit)
+        {
+            if (_audit.UserLogID != null) this.UserLogID =(Guid) _audit.UserLogID;
+            if (_audit.UserID != null) this.UserID =(Guid) _audit.UserID;
+            if (_audit.ActionType != null) this.ActionType = (String)_audit.ActionType;
+            if (_audit.ActionTime != Convert.ToDateTime("01/01/0001")) this.ActionTime = (DateTime)_audit.ActionTime;
+            if (_audit.ActionValue != null) this.ActionValue = (string)_audit.ActionValue;
+        
+        }
+
+        [DataMember]
+        public Guid UserLogID { get; set; }
+
+        [DataMember]
+        public Guid UserID { get; set; }
+
+        [DataMember]
+        public String ActionType { get; set; }
+
+        [DataMember]
+        public DateTime ActionTime { get; set; }
+
+        [DataMember]
+        public string ActionValue { get; set; }
+    }
+}
---
+++ KrausWarehouseServices/KrausWarehouseServices/DTO/Shipping/BoxPackageDTO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KrausWarehouseServices.Connections;
+using System.Runtime.Serialization;
+
+namespace KrausWarehouseServices.DTO.Shipping
+{
+    [DataContract]
+   public class BoxPackageDTO
+    {
+       /// <summary>
+       /// Blank Conctructor
+       /// </summary>
+       public BoxPackageDTO()
+       { 
+       
+       }
+
+       /// <summary>
+       /// Parameterised Constructor
+       /// </summary>
+       /// <param name="_boxpackage">
+       /// Pass the BoxPackage Object.
+       /// </param>
+       public BoxPackageDTO(Connections.Shipping.BoxPackage _boxpackage)
+       {
+
+           if (_boxpackage.BoxID != null) this.BoxID = (Guid)_boxpackage.BoxID;
+       
+
+      
+
+       }
+
+       public Guid BoxID { get; set; }
+       public Guid PackingID { get; set; }
+       public string BoxType { get; set; }
+       public Double BoxWeight { get; set; }
+       public Double BoxLength { get; set; }
+       public Double BoxHeight { get; set; }
+       public Double BoxWidth { get; set; }
+       public DateTime BoxCreatedTime { get; set; }
+       public DateTime BoxMeasurementTime { get; set; }
+       public int ROWID { get; set; }
+       public string BOXNUM { get; set; }
+    }
+}
