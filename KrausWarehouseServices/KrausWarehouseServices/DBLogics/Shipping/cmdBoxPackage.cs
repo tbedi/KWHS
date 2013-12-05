51b241f2d6b852256145f232b8349ce78c5b6107
Avinash <apatil@kraususa.com>
2013-12-05 20:20:05 +0530

    Box package table Commands Completed

---
+++ KrausWarehouseServices/KrausWarehouseServices/DBLogics/Shipping/cmdBoxPackage.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KrausWarehouseServices.DTO;
+
+namespace KrausWarehouseServices.DBLogics.Shipping
+{
+   public class cmdBoxPackage
+    {
+       
+       /// <summary>
+       /// Shipping Database class object.
+       /// </summary>
+       Connections.Shipping.Shipping_ManagerEntities1 entShipping = new Connections.Shipping.Shipping_ManagerEntities1();
+
+       #region Get Fucntions
+
+       /// <summary>
+       /// Get all BoxPackage Table Information
+       /// </summary>
+       /// <returns>List<DTO.Shipping.BoxPackageDTO></returns>
+       public List<DTO.Shipping.BoxPackageDTO> GetAll()
+       {
+           List<DTO.Shipping.BoxPackageDTO> lsreturn = new List<DTO.Shipping.BoxPackageDTO>();
+           try
+           {
+               var allBoxInfo = from b in entShipping.BoxPackages
+                                select b;
+
+               foreach (var _boxitem in allBoxInfo)
+               {
+
+                   lsreturn.Add(new DTO.Shipping.BoxPackageDTO(_boxitem));
+               }
+           }
+           catch (Exception)
+           { }
+           return lsreturn;
+
+       }
+
+       /// <summary>
+       /// Selected single Row by filter Box id
+       /// </summary>
+       /// <param name="BoxID">Guid Box ID</param>
+       /// <returns>DTO.Shipping.BoxPackageDTO Object</returns>
+       public DTO.Shipping.BoxPackageDTO GetSelectedByBoxID(Guid BoxID)
+       {
+           DTO.Shipping.BoxPackageDTO _return = new DTO.Shipping.BoxPackageDTO();
+           try
+           {
+              Connections.Shipping.BoxPackage _boxitem = entShipping.BoxPackages.SingleOrDefault(i => i.BoxID == BoxID);
+
+              DTO.Shipping.BoxPackageDTO _box = new DTO.Shipping.BoxPackageDTO(_boxitem);
+               _return = _box;
+           }
+           catch (Exception)
+           { }
+           return _return;
+       }
+
+       /// <summary>
+       /// Get BoxPackage Table information sorted by BOXNUM
+       /// </summary>
+       /// <param name="BoxNumber">String Box Nuber</param>
+       /// <returns>BoxPackage Object </returns>
+       public DTO.Shipping.BoxPackageDTO GetSelectedByBoxNumber(String BoxNumber)
+       {
+           DTO.Shipping.BoxPackageDTO _return = new DTO.Shipping.BoxPackageDTO();
+           try
+           {
+               _return = new DTO.Shipping.BoxPackageDTO(entShipping.BoxPackages.SingleOrDefault(i => i.BOXNUM == BoxNumber));
+
+           }
+           catch (Exception)
+           { }
+           return _return;
+       }
+
+       /// <summary>
+       /// list of information by Packing ID
+       /// </summary>
+       /// <param name="PackingID">Guid Packing ID</param>
+       /// <returns>List<DTO.Shipping.BoxPackageDTO> </returns>
+       public List<DTO.Shipping.BoxPackageDTO> GetSelectedByPackingID(Guid PackingID)
+       {
+           List<DTO.Shipping.BoxPackageDTO> lsreturn = new List<DTO.Shipping.BoxPackageDTO>();
+           try
+           {
+               var allBoxInfo = from b in entShipping.BoxPackages
+                                where b.PackingID == PackingID
+                                select b;
+
+               foreach (var _boxitem in allBoxInfo)
+               {
+                   lsreturn.Add(new DTO.Shipping.BoxPackageDTO(_boxitem));
+               }
+           }
+           catch (Exception)
+           { }
+           return lsreturn;
+       }
+
+       #endregion
+
+       #region Set Functions
+
+       /// <summary>
+       /// Save Box information to the database
+       /// </summary>
+       /// <param name="lsBoxpackage">list of information of box</param>
+       /// <returns>Guid of New Box Id</returns>
+       public Guid SaveBoxPackage(List<DTO.Shipping.BoxPackageDTO> lsBoxpackage)
+       {
+           Guid _return = Guid.Empty;
+
+           try
+           {
+               foreach (var _boxitem in lsBoxpackage)
+               {
+                   Connections.Shipping.BoxPackage _boxPackage = new Connections.Shipping.BoxPackage();
+                   _boxPackage.BoxID = Guid.NewGuid();
+                   _boxPackage.PackingID = _boxitem.PackingID;
+                   _boxPackage.BoxType = _boxitem.BoxType;
+                   _boxPackage.BoxWeight = _boxitem.BoxWeight;
+                   _boxPackage.BoxLength = _boxitem.BoxLength;
+                   _boxPackage.BoxHeight = _boxitem.BoxHeight;
+                   _boxPackage.BoxWidth = _boxitem.BoxWidth;
+                   _boxPackage.BoxCreatedTime = _boxitem.BoxCreatedTime;
+                   if (_boxitem.BoxMeasurementTime.Date != Convert.ToDateTime("01/01/0001").Date) _boxPackage.BoxMeasurementTime = _boxitem.BoxMeasurementTime;
+                   entShipping.AddToBoxPackages(_boxPackage);
+                   _return = _boxPackage.BoxID;
+               }
+               entShipping.SaveChanges();
+           }
+           catch (Exception)
+           { }
+           return _return;
+       }
+
+       #endregion
+    }
+}
--- KrausWarehouseServices/KrausWarehouseServices/DTO/Shipping/BoxPackageDTO.cs
+++ KrausWarehouseServices/KrausWarehouseServices/DTO/Shipping/BoxPackageDTO.cs
@@ -9,70 +9,70 @@ using System.Runtime.Serialization;
 namespace KrausWarehouseServices.DTO.Shipping
 {
     [DataContract]
-   public class BoxPackageDTO
+    public class BoxPackageDTO
     {
-       /// <summary>
-       /// Blank Conctructor
-       /// </summary>
-       public BoxPackageDTO()
-       { 
        
-       } 
-        
-        [DataMember]
-       public Guid BoxID { get; set; }
 
         [DataMember]
-       public Guid PackingID { get; set; }
+        public Guid BoxID { get; set; }
 
         [DataMember]
-       public string BoxType { get; set; }
+        public Guid PackingID { get; set; }
 
         [DataMember]
-       public Double BoxWeight { get; set; }
+        public string BoxType { get; set; }
 
         [DataMember]
-       public Double BoxLength { get; set; }
+        public Double BoxWeight { get; set; }
 
         [DataMember]
-       public Double BoxHeight { get; set; }
+        public Double BoxLength { get; set; }
 
         [DataMember]
-       public Double BoxWidth { get; set; }
+        public Double BoxHeight { get; set; }
 
         [DataMember]
-       public DateTime BoxCreatedTime { get; set; }
+        public Double BoxWidth { get; set; }
 
         [DataMember]
-       public DateTime BoxMeasurementTime { get; set; }
+        public DateTime BoxCreatedTime { get; set; }
 
         [DataMember]
-       public int ROWID { get; set; }
+        public DateTime BoxMeasurementTime { get; set; }
 
         [DataMember]
-       public string BOXNUM { get; set; }
+        public int ROWID { get; set; }
 
-       /// <summary>
-       /// Parameterised Constructor
-       /// </summary>
-       /// <param name="_boxpackage">
-       /// Pass the BoxPackage Object.
-       /// </param>
-       public BoxPackageDTO(Connections.Shipping.BoxPackage _boxpackage)
-       {
-           if (_boxpackage.BoxID != null) this.BoxID = (Guid)_boxpackage.BoxID;
-           if (_boxpackage.PackingID != null) this.PackingID = (Guid)_boxpackage.PackingID;
-           if (_boxpackage.BoxWeight != null) this.BoxWeight = (Double)_boxpackage.BoxWeight;
-           if (_boxpackage.BoxLength != null) this.BoxLength = (Double)_boxpackage.BoxLength;
-           if(_boxpackage.BoxHeight!=null) this.BoxHeight=(Double)_boxpackage.BoxHeight;
-           if (_boxpackage.BoxWidth != null) this.BoxWidth = (double)_boxpackage.BoxWidth;
-           if(_boxpackage.BoxCreatedTime!=Convert.ToDateTime("01/01/0001"))this.BoxCreatedTime=(DateTime)_boxpackage.BoxCreatedTime;
-           if (_boxpackage.BoxMeasurementTime != Convert.ToDateTime("01/01/0001")) this.BoxMeasurementTime = (DateTime)_boxpackage.BoxMeasurementTime;
-           this.ROWID = _boxpackage.ROWID;
-           if (_boxpackage.BOXNUM != null) this.BOXNUM = (String)_boxpackage.BOXNUM;
+        [DataMember]
+        public string BOXNUM { get; set; }
 
-       }
+        /// <summary>
+        /// Blank Conctructor
+        /// </summary>
+        public BoxPackageDTO()
+        {
 
-      
+        }
+
+        /// <summary>
+        /// Parameterised Constructor
+        /// </summary>
+        /// <param name="_boxpackage">
+        /// Pass the BoxPackage Object.
+        /// </param>
+        public BoxPackageDTO(Connections.Shipping.BoxPackage _boxpackage)
+        {
+            if (_boxpackage.BoxID != null) this.BoxID = (Guid)_boxpackage.BoxID;
+            if (_boxpackage.PackingID != null) this.PackingID = (Guid)_boxpackage.PackingID;
+            if (_boxpackage.BoxWeight != null) this.BoxWeight = (Double)_boxpackage.BoxWeight;
+            if (_boxpackage.BoxLength != null) this.BoxLength = (Double)_boxpackage.BoxLength;
+            if (_boxpackage.BoxHeight != null) this.BoxHeight = (Double)_boxpackage.BoxHeight;
+            if (_boxpackage.BoxWidth != null) this.BoxWidth = (double)_boxpackage.BoxWidth;
+            if (_boxpackage.BoxCreatedTime != Convert.ToDateTime("01/01/0001")) this.BoxCreatedTime = (DateTime)_boxpackage.BoxCreatedTime;
+            if (_boxpackage.BoxMeasurementTime != Convert.ToDateTime("01/01/0001")) this.BoxMeasurementTime = (DateTime)_boxpackage.BoxMeasurementTime;
+            this.ROWID = _boxpackage.ROWID;
+            if (_boxpackage.BOXNUM != null) this.BOXNUM = (String)_boxpackage.BOXNUM;
+
+        }
     }
 }
