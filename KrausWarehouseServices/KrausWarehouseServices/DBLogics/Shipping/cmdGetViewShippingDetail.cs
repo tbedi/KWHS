5ccaaa5714ce3f845864dcfdbebf258e3e1382ce
Shriram <Shriram@kraususa.com>
2013-12-06 22:47:20 +0530

    Added all get methods in cmdviewshippingDetail

---
+++ KrausWarehouseServices/KrausWarehouseServices/DBLogics/Shipping/cmdGetViewShippingDetail.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KrausWarehouseServices.Connections.Shipping;
+using KrausWarehouseServices.DTO.Shipping;
+
+namespace KrausWarehouseServices.DBLogics.Shipping
+{
+   public class cmdGetViewShippingDetail
+    {
+       /// <summary>
+       /// Create the Object OF Shipping Entity.
+       /// </summary>
+       Shipping_ManagerEntities1 entshipping = new Shipping_ManagerEntities1();
+
+       /// <summary>
+       /// Get all Records from View Shipping Detail.
+       /// </summary>
+       /// <returns>
+       /// Return The List of all Shipping Detail.
+       /// </returns>
+       public List<viewgetShippingDetails> GetAll()
+       {
+           List<viewgetShippingDetails> _lsViewShippingdetail = new List<viewgetShippingDetails>();
+           try
+           {
+               var viewShip = (from viewshippingdetail in entshipping.getShippingDetails
+                               select viewshippingdetail).ToList();
+
+               foreach (var viewShippingitem in viewShip)
+               {
+                   _lsViewShippingdetail.Add(new viewgetShippingDetails(viewShippingitem));
+               }
+           }
+           catch (Exception)
+           {
+           }
+           return _lsViewShippingdetail;
+       
+       }
+
+       /// <summary>
+       /// Get Shipping Details By ShipmentNum.
+       /// </summary>
+       /// <param name="ShipmentNum">
+       /// pass Shipment Num.
+       /// </param>
+       /// <returns>
+       /// return list.
+       /// </returns>
+       public List<viewgetShippingDetails> GetByShipmentNum(String ShipmentNum)
+       {
+           List<viewgetShippingDetails> _lsviewShipmentDetail = new List<viewgetShippingDetails>();
+           try
+           {
+               var ship = (from viewdetail in entshipping.getShippingDetails
+                           where viewdetail.ShippingNum == ShipmentNum
+                           select viewdetail).ToList();
+
+               foreach (var shippingitem in ship)
+               {
+                   _lsviewShipmentDetail.Add(new viewgetShippingDetails(shippingitem));
+               }
+           }
+           catch (Exception)
+           {
+           }
+           return _lsviewShipmentDetail;
+       }
+
+       /// <summary>
+       /// Get Shipping Details By OrderID
+       /// </summary>
+       /// <param name="_orderID"></param>
+       /// <returns></returns>
+       public List<viewgetShippingDetails> GetByOrderID(String _orderID)
+       {
+           List<viewgetShippingDetails> _lsviewShipmentDetail = new List<viewgetShippingDetails>();
+           try
+           {
+               var ship = (from viewdetail in entshipping.getShippingDetails
+                           where viewdetail.OrderID == _orderID
+                           select viewdetail).ToList();
+
+               foreach (var shippingitem in ship)
+               {
+                   _lsviewShipmentDetail.Add(new viewgetShippingDetails(shippingitem));
+               }
+           }
+           catch (Exception)
+           {
+           }
+           return _lsviewShipmentDetail;
+       }
+
+       /// <summary>
+       /// Get Shipping Details By ponum
+       /// </summary>
+       /// <param name="_ponumber">
+       /// pass Ponum as parameter.
+       /// </param>
+       /// <returns>
+       /// return List.
+       /// </returns>
+       public List<viewgetShippingDetails> GetByOrderID(String _ponumber)
+       {
+           List<viewgetShippingDetails> _lsviewShipmentDetail = new List<viewgetShippingDetails>();
+           try
+           {
+               var ship = (from viewdetail in entshipping.getShippingDetails
+                           where viewdetail.CustomerPO == _ponumber
+                           select viewdetail).ToList();
+
+               foreach (var shippingitem in ship)
+               {
+                   _lsviewShipmentDetail.Add(new viewgetShippingDetails(shippingitem));
+               }
+           }
+           catch (Exception)
+           {
+           }
+           return _lsviewShipmentDetail;
+       }
+
+
+       /// <summary>
+       /// Get Shipping Details By supplierNo.
+       /// </summary>
+       /// <param name="_suppilerNo">
+       /// pass suppilerNo as parameter.
+       /// </param>
+       /// <returns>
+       /// return List.
+       /// </returns>
+       public List<viewgetShippingDetails> GetByOrderID(String _suppilerNo)
+       {
+           List<viewgetShippingDetails> _lsviewShipmentDetail = new List<viewgetShippingDetails>();
+           try
+           {
+               var ship = (from viewdetail in entshipping.getShippingDetails
+                           where viewdetail.OurSupplierNo == _suppilerNo
+                           select viewdetail).ToList();
+
+               foreach (var shippingitem in ship)
+               {
+                   _lsviewShipmentDetail.Add(new viewgetShippingDetails(shippingitem));
+               }
+           }
+           catch (Exception)
+           {
+           }
+           return _lsviewShipmentDetail;
+       }
+
+       /// <summary>
+       /// Get Shipping Details By vendername.
+       /// </summary>
+       /// <param name="_vendername">
+       /// pass vendername as parameter.
+       /// </param>
+       /// <returns>
+       /// return List.
+       /// </returns>
+       public List<viewgetShippingDetails> GetByOrderID(String _vendername)
+       {
+           List<viewgetShippingDetails> _lsviewShipmentDetail = new List<viewgetShippingDetails>();
+           try
+           {
+               var ship = (from viewdetail in entshipping.getShippingDetails
+                           where viewdetail.VendorName == _vendername
+                           select viewdetail).ToList();
+
+               foreach (var shippingitem in ship)
+               {
+                   _lsviewShipmentDetail.Add(new viewgetShippingDetails(shippingitem));
+               }
+           }
+           catch (Exception)
+           {
+           }
+           return _lsviewShipmentDetail;
+       }
+    }
+}
