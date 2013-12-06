9caf57a99856bf6dde641805358ac976c95a0fae
Avinash <apatil@kraususa.com>
2013-12-06 21:03:15 +0530

    Get Shipment Data commands completed

---
+++ KrausWarehouseServices/KrausWarehouseServices/DBLogics/Shipping/cmdGetShippingData.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrausWarehouseServices.DBLogics.Shipping
+{
+    public class cmdGetShippingData
+    {
+        /// <summary>
+        /// Shipping Manager Database entity Object.
+        /// </summary>
+        Connections.Shipping.Shipping_ManagerEntities1 entShipping = new Connections.Shipping.Shipping_ManagerEntities1();
+
+
+        /// <summary>
+        /// Get all records from Get_Shipping_Data view.
+        /// </summary>
+        /// <returns>
+        /// list of DTO object.
+        /// </returns>
+        public List<DTO.Shipping.viewGet_Shipping_DataDTO> GetAll()
+        {
+            List<DTO.Shipping.viewGet_Shipping_DataDTO> _lsReturn = new List<DTO.Shipping.viewGet_Shipping_DataDTO>();
+            try
+            {
+                var ShippingData = from _ship in entShipping.Get_Shipping_Data select _ship;
+
+                foreach (var _xShippingitem in ShippingData)
+                {
+                    _lsReturn.Add(new DTO.Shipping.viewGet_Shipping_DataDTO(_xShippingitem));
+                }
+            }
+            catch (Exception)
+            { }
+            return _lsReturn;
+        }
+
+
+        /// <summary>
+        /// Get by Shipment Number.
+        /// </summary>
+        /// <param name="ShipmentID">
+        /// String Shipment Number.
+        /// </param>
+        /// <returns></returns>
+        public List<DTO.Shipping.viewGet_Shipping_DataDTO> GeByShipmentID(String ShipmentID)
+        {
+            List<DTO.Shipping.viewGet_Shipping_DataDTO> _lsReturn = new List<DTO.Shipping.viewGet_Shipping_DataDTO>();
+            try
+            {
+                var ShippingData = (from _ship in entShipping.Get_Shipping_Data
+                                    where _ship.ShipmentID == ShipmentID
+                                    select _ship).ToList();
+
+                foreach (var _xShippingitem in ShippingData)
+                {
+                    _lsReturn.Add(new DTO.Shipping.viewGet_Shipping_DataDTO(_xShippingitem));
+                }
+            }
+            catch (Exception)
+            { }
+            return _lsReturn;
+        }
+
+
+        /// <summary>
+        /// Get all view recoreds filtered by Location.
+        /// </summary>
+        /// <param name="LocationName">
+        /// String Location.
+        /// </param>
+        /// <returns></returns>
+        public List<DTO.Shipping.viewGet_Shipping_DataDTO> GeByLocation(String LocationName)
+        {
+            List<DTO.Shipping.viewGet_Shipping_DataDTO> _lsReturn = new List<DTO.Shipping.viewGet_Shipping_DataDTO>();
+            try
+            {
+                var ShippingData = (from _ship in entShipping.Get_Shipping_Data
+                                    where _ship.LocationCombined == LocationName
+                                    select _ship).ToList();
+
+                foreach (var _xShippingitem in ShippingData)
+                {
+                    _lsReturn.Add(new DTO.Shipping.viewGet_Shipping_DataDTO(_xShippingitem));
+                }
+            }
+            catch (Exception)
+            { }
+            return _lsReturn;
+        }
+
+    }
+}
