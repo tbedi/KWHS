86b60375bcb90f9f3b3204579185974cb0063763
Avinash <apatil@kraususa.com>
2013-12-07 00:21:12 +0530

    Shipping Get and Save services added

---
+++ KrausWarehouseServices/KrausWarehouseServices/Service/Shipping/Get.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace KrausWarehouseServices.Service.Shipping
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Get" in both code and config file together.
+    public class Get : IGet
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
