39dc819f1cbfe866d64caf645589302a819d3d4f
Shriram <Shriram@kraususa.com>
2013-12-16 16:58:04 +0530

    Change Object Name

--- KrausWarehouseServices/KrausWarehouseServices/Service/Shipping/Save.cs
+++ KrausWarehouseServices/KrausWarehouseServices/Service/Shipping/Save.cs
@@ -21,57 +21,57 @@ namespace KrausWarehouseServices.Service.Shipping
         /// <summary>
         /// cmdAudit new Object. 
         /// </summary>
-        DBLogics.Shipping.cmdAudit _GetAudit = new DBLogics.Shipping.cmdAudit();
+        DBLogics.Shipping.cmdAudit _returnAudit = new DBLogics.Shipping.cmdAudit();
 
         /// <summary>
         /// cmdpackage new object.
         /// </summary>
-        DBLogics.Shipping.cmdPackage _GetPackage = new DBLogics.Shipping.cmdPackage();
+        DBLogics.Shipping.cmdPackage _returnPackage = new DBLogics.Shipping.cmdPackage();
 
         /// <summary>
         /// cmdpackagedetail New Object.
         /// </summary>
-        DBLogics.Shipping.cmdPackageDetail _GetPackageDetail = new DBLogics.Shipping.cmdPackageDetail();
+        DBLogics.Shipping.cmdPackageDetail _returnPackageDetail = new DBLogics.Shipping.cmdPackageDetail();
 
         /// <summary>
         ///cmdRole New Object. 
         /// </summary>
-        DBLogics.Shipping.cmdRole _GetRole = new DBLogics.Shipping.cmdRole();
+        DBLogics.Shipping.cmdRole _returnRole = new DBLogics.Shipping.cmdRole();
 
         /// <summary>
         /// CmdShipping New Object.
         /// </summary>
-        DBLogics.Shipping.cmdShipping _GetShipping = new DBLogics.Shipping.cmdShipping();
+        DBLogics.Shipping.cmdShipping _returnShipping = new DBLogics.Shipping.cmdShipping();
 
         /// <summary>
         /// cmdStationMaster New Object.
         /// </summary>
-        DBLogics.Shipping.cmdStationMaster _GetStation = new DBLogics.Shipping.cmdStationMaster();
+        DBLogics.Shipping.cmdStationMaster _returnStation = new DBLogics.Shipping.cmdStationMaster();
 
         /// <summary>
         /// cmdtracking New Object.
         /// </summary>
-        DBLogics.Shipping.cmdTracking _GetTracking = new DBLogics.Shipping.cmdTracking();
+        DBLogics.Shipping.cmdTracking _returnTracking = new DBLogics.Shipping.cmdTracking();
 
         /// <summary>
         /// cmduser new Object.
         /// </summary>
-        DBLogics.Shipping.cmdUser _GetUser = new DBLogics.Shipping.cmdUser();
+        DBLogics.Shipping.cmdUser _returnUser = new DBLogics.Shipping.cmdUser();
 
         /// <summary>
         /// cmduserstation new Object.
         /// </summary>
-        DBLogics.Shipping.cmdUserStation _GetUserStation = new DBLogics.Shipping.cmdUserStation();
+        DBLogics.Shipping.cmdUserStation _returnUserStation = new DBLogics.Shipping.cmdUserStation();
 
         /// <summary>
         /// cmdErrorLog new Object.
         /// </summary>
-        DBLogics.Shipping.cmdErrorLog _GetErrorLog = new DBLogics.Shipping.cmdErrorLog();
+        DBLogics.Shipping.cmdErrorLog _returnErrorLog = new DBLogics.Shipping.cmdErrorLog();
 
         /// <summary>
         /// cmdboxpackage new Object.
         /// </summary>
-        DBLogics.Shipping.cmdBoxPackage _GetBoxPackage = new DBLogics.Shipping.cmdBoxPackage();
+        DBLogics.Shipping.cmdBoxPackage _returnBoxPackage = new DBLogics.Shipping.cmdBoxPackage();
 
         #endregion
 
@@ -79,52 +79,52 @@ namespace KrausWarehouseServices.Service.Shipping
 
         public bool Package(List<DTO.Shipping.PackageDTO> _package)
         {
-            return _GetPackage.UpsertPackage(_package);
+            return _returnPackage.UpsertPackage(_package);
         }
 
         public bool PackageDetail(List<DTO.Shipping.PackageDetailDTO> _packagedetail)
         {
-            return _GetPackageDetail.UpsertPackageDetails(_packagedetail);
+            return _returnPackageDetail.UpsertPackageDetails(_packagedetail);
         }
 
         public bool Role(List<DTO.Shipping.RoleDTO> _role)
         {
-            return _GetRole.UpsertRole(_role);
+            return _returnRole.UpsertRole(_role);
         }
         public bool Shipping(List<DTO.Shipping.ShippingDTO> _shipping)
         {
-            return _GetShipping.UpsertShipping(_shipping);
+            return _returnShipping.UpsertShipping(_shipping);
         }
 
         public bool StationMaster(List<DTO.Shipping.StationMasterDTO> _stationmaster)
         {
-            return _GetStation.UpsertStation(_stationmaster);
+            return _returnStation.UpsertStation(_stationmaster);
         }
 
         public bool User(List<DTO.Shipping.UserDTO> _user)
         {
-            return _GetUser.UsertUser(_user);
+            return _returnUser.UsertUser(_user);
         }
 
         public bool UserStation(List<DTO.Shipping.UserStationDTO> _userstation)
         {
-            return _GetUserStation.UpsertUserStation(_userstation);
+            return _returnUserStation.UpsertUserStation(_userstation);
         }
 
         public bool ErrorLog(List<DTO.Shipping.ErrorLogDTO> _errorlog)
         {
-            return _GetErrorLog.UpsertErrorLog(_errorlog);
+            return _returnErrorLog.UpsertErrorLog(_errorlog);
         }
 
         public bool Audit(List<DTO.Shipping.AutditDTO> _audit)
         {
-            return _GetAudit.UpsertAuditLog(_audit);
+            return _returnAudit.UpsertAuditLog(_audit);
         }
 
 
         public bool BoxPackage(List<DTO.Shipping.BoxPackageDTO> _boxpackage)
         {
-            return _GetBoxPackage.UpsertBoxPackage(_boxpackage);
+            return _returnBoxPackage.UpsertBoxPackage(_boxpackage);
         }
     }
 }
