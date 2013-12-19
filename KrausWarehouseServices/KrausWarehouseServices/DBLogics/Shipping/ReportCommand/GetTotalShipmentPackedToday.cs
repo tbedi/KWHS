ee59c8bf6e8242d4fec64c3a17bdccfcf829a104
Shriram <Shriram@kraususa.com>
2013-12-19 20:30:06 +0530

    ReportCommand added in services Shipping

--- KrausWarehouseServices/KrausWarehouseServices/DBLogics/Shipping/ReportCommand/GetTotalShipmentPackedToday.cs
+++ KrausWarehouseServices/KrausWarehouseServices/DBLogics/Shipping/ReportCommand/GetTotalShipmentPackedToday.cs
@@ -1,52 +1,55 @@
-﻿using System;
+﻿using KrausWarehouseServices.DTO.Shipping.ReportEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KrausWarehouseServices.Connections.Shipping;
+using System.Data.Objects;
 
 namespace KrausWarehouseServices.DBLogics.Shipping.ReportCommand
 {
     public class GetTotalShipmentPackedToday
     {
+        Shipping_ManagerEntities1 entShipping = new Shipping_ManagerEntities1();
+
+        /// <summary>
+        /// Current date packing details
+        /// </summary>
+        /// <returns></returns>
+        public List<ShipmentPackedTodayAndAvgTimeDTO> GetTotalShipmentPackedTime()
+        {
+            List<ShipmentPackedTodayAndAvgTimeDTO> lsShipmentPacked = new List<ShipmentPackedTodayAndAvgTimeDTO>();
+            try
+            {
+                String CurrentTime = DateTime.UtcNow.ToString();
+
+                var packingCount = from user in entShipping.Users //Service.Get.UserAllUser()
+                                   join packing in entShipping.Packages //Service.Get.PackageAllPackge()
+                                   on user.UserID equals packing.UserId
+                                   where EntityFunctions.TruncateTime(packing.EndTime) == EntityFunctions.TruncateTime(DateTime.UtcNow)
+                                   group packing by packing.UserId into Gpacking
+                                   select new
+                                   {
+                                       userID = Gpacking.Key,
+                                       TotalPacked = Gpacking.Count(i => i.PackingStatus == 0)
+
+                                   };
 
 
-        ///// <summary>
-        ///// Current date packing details
-        ///// </summary>
-        ///// <returns></returns>
-        //public List<cstShipmentPackedTodayAndAvgTime> GetTotalShipmentPackedTime()
-        //{
-        //    List<cstShipmentPackedTodayAndAvgTime> lsShipmentPacked = new List<cstShipmentPackedTodayAndAvgTime>();
-        //    try
-        //    {
-        //        String CurrentTime = DateTime.UtcNow.ToString();
 
-        //        var packingCount = from user in Service.Get.UserAllUser()
-        //                           join packing in Service.Get.PackageAllPackge()
-        //                           on user.UserID equals packing.UserID
-        //                           where EntityFunctions.TruncateTime(packing.EndTime) == EntityFunctions.TruncateTime(DateTime.UtcNow)
-        //                           group packing by packing.UserID into Gpacking
-        //                           select new
-        //                           {
-        //                               userID = Gpacking.Key,
-        //                               TotalPacked = Gpacking.Count(i => i.PackingStatus == 0)
+                foreach (var pckitem in packingCount)
+                {
+                    ShipmentPackedTodayAndAvgTimeDTO _cspck = new ShipmentPackedTodayAndAvgTimeDTO();
+                    _cspck.UserID = pckitem.userID;
+                    _cspck.Packed = Convert.ToInt32(pckitem.TotalPacked);
+                    lsShipmentPacked.Add(_cspck);
+                }
 
-        //                           };
-
-
-
-        //        foreach (var pckitem in packingCount)
-        //        {
-        //            cstShipmentPackedTodayAndAvgTime _cspck = new cstShipmentPackedTodayAndAvgTime();
-        //            _cspck.UserID = pckitem.userID;
-        //            _cspck.Packed = Convert.ToInt32(pckitem.TotalPacked);
-        //            lsShipmentPacked.Add(_cspck);
-        //        }
-
-        //    }
-        //    catch (Exception)
-        //    { }
-        //    return lsShipmentPacked;
-        //}
+            }
+            catch (Exception)
+            { }
+            return lsShipmentPacked;
+        }
     }
 }
--- KrausWarehouseServices/KrausWarehouseServices/DBLogics/Shipping/ReportCommand/cmdUserCurrentStationAndDeviceID.cs
+++ KrausWarehouseServices/KrausWarehouseServices/DBLogics/Shipping/ReportCommand/cmdUserCurrentStationAndDeviceID.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KrausWarehouseServices.Connections.Shipping;
+using KrausWarehouseServices.DTO.Shipping.ReportEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,54 +11,56 @@ namespace KrausWarehouseServices.DBLogics.Shipping.ReportCommand
     public class cmdUserCurrentStationAndDeviceID
     {
 
-        ///// <summary>
-        ///// All Users last Login Station
-        ///// </summary>
-        ///// <returns></returns>
-        //public List<cstUserCurrentStationAndDeviceID> LastLoginStationAllUsers()
-        //{
-        //    List<cstUserCurrentStationAndDeviceID> lsUstation = new List<cstUserCurrentStationAndDeviceID>();
-        //    try
-        //    {
-        //        var UserName = from user in Service.Get.UserAllUser()
-        //                       join Us in Service.Get.UserStationAllUser()
-        //                       on user.UserID equals Us.UserID
-        //                       group Us by user.UserID into Gusers
-        //                       select new
-        //                       {
-        //                           User = Gusers.Key,
-        //                           StationTime = Gusers.Max(i => i.LoginDateTime),
-        //                           StaionID = Gusers.FirstOrDefault(i => i.UserID == Gusers.Key && i.LoginDateTime == Gusers.Max(j => j.LoginDateTime)).StationID
-        //                       };
-        //        var StaionName = from Station in UserName
-        //                         join Station2 in Service.Get.StationMasterAllStation()
-        //                       on Station.StaionID equals Station2.StationID
-        //                         join User in Service.Get.UserAllUser()
-        //                         on Station.User equals User.UserID
-        //                         select new
-        //                         {
-        //                             UserID = User.UserID,
-        //                             UserName = User.UserFullName,
-        //                             Station.StationTime,
-        //                             Station2.StationName,
-        //                             Station2.DeviceNumber,
-        //                         };
+        Shipping_ManagerEntities1 entshipping = new Shipping_ManagerEntities1();
 
-        //        foreach (var Useritem in StaionName)
-        //        {
-        //            cstUserCurrentStationAndDeviceID UserStation = new cstUserCurrentStationAndDeviceID();
-        //            UserStation.UserID = Useritem.UserID;
-        //            UserStation.UserName = Useritem.UserName;
-        //            UserStation.StationName = Useritem.StationName;
-        //            UserStation.Datetime = Useritem.StationTime.ToString("MMM dd, yyyy hh:mm tt");
-        //            UserStation.DeviceID = Useritem.DeviceNumber;
-        //            lsUstation.Add(UserStation);
-        //        }
-        //    }
-        //    catch (Exception)
-        //    { }
-        //    return lsUstation;
-        //}
+        /// <summary>
+        /// All Users last Login Station
+        /// </summary>
+        /// <returns></returns>
+        public List<UserCurrentStationAndDeviceIDDTO> LastLoginStationAllUsers()
+        {
+            List<UserCurrentStationAndDeviceIDDTO> lsUstation = new List<UserCurrentStationAndDeviceIDDTO>();
+            try
+            {
+                var UserName = from user in entshipping.Users//Service.Get.UserAllUser()
+                               join Us in entshipping.UserStations//Service.Get.UserStationAllUser()
+                               on user.UserID equals Us.UserID
+                               group Us by user.UserID into Gusers
+                               select new
+                               {
+                                   User = Gusers.Key,
+                                   StationTime = Gusers.Max(i => i.LoginDateTime),
+                                   StaionID = Gusers.FirstOrDefault(i => i.UserID == Gusers.Key && i.LoginDateTime == Gusers.Max(j => j.LoginDateTime)).StationID
+                               };
+                var StaionName = from Station in UserName
+                                 join Station2 in entshipping.Stations //Service.Get.StationMasterAllStation()
+                               on Station.StaionID equals Station2.StationID
+                                 join User in entshipping.Users //Service.Get.UserAllUser()
+                                 on Station.User equals User.UserID
+                                 select new
+                                 {
+                                     UserID = User.UserID,
+                                     UserName = User.UserFullName,
+                                     Station.StationTime,
+                                     Station2.StationName,
+                                     Station2.DeviceNumber,
+                                 };
+
+                foreach (var Useritem in StaionName)
+                {
+                    UserCurrentStationAndDeviceIDDTO UserStation = new UserCurrentStationAndDeviceIDDTO();
+                    UserStation.UserID = Useritem.UserID;
+                    UserStation.UserName = Useritem.UserName;
+                    UserStation.StationName = Useritem.StationName;
+                    UserStation.Datetime = Useritem.StationTime.ToString("MMM dd, yyyy hh:mm tt");
+                    UserStation.DeviceID = Useritem.DeviceNumber;
+                    lsUstation.Add(UserStation);
+                }
+            }
+            catch (Exception)
+            { }
+            return lsUstation;
+        }
 
     }
 }
